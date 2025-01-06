f46b51afddfa2d0bc0a35482b9dee2735e6ce5f5
Ivanclourenco <ivancampos039@gmail.com>
2025-01-06 16:34:04 -0300

    Frases motivacionais

--- reactaula/src/App.tsx
+++ reactaula/src/App.tsx
@@ -1,9 +1,41 @@
-//import{ useState } from "react";
+import{ useState } from "react";
 import './App.css'
 import logoImg from "./assets/logo.png"
 
 function App() {
-  
+  const [textofrase, setTextofrase] = useState("")
+  const [categoriaSelecionada, setCategoriaSelecionada] = useState(0)
+
+  const allFrases = [
+    {
+      id:1,
+      nome:"Motivação	",
+      frases: [
+        "Se você não tentar você não ganha",
+        "Se liga nessa play",
+        "Marcos vai jogar de Homem-aranha",
+        "To AFK base não tem Healer"
+
+      ]
+    },
+    {
+      id:2,
+      nome:"Bom dia",
+      frases:[
+        "Bom dia ponte da bandeirantes",
+        "Você tem 2 minutinhos e coisa rapida?",
+        "Aqui não esta funcionando",
+        "Você consegue colocar esse botão na cor roxa?"
+      ]
+    }
+  ]
+  function handleSwitchCategory(index:number){
+    setCategoriaSelecionada(index);
+  }
+  function gerarFrase(){
+    const numeroAleatorio =Math.floor(Math.random() * allFrases[categoriaSelecionada].frases.length)
+    setTextofrase(`"${allFrases[categoriaSelecionada].frases[numeroAleatorio]}"`);
+  }
 
   return (
    
@@ -12,13 +44,27 @@ function App() {
 
         <h2 className="title">Categorias</h2>
         <section className="category-area">
-          <button className="category-button">Motivação</button>
-          <button className="category-button">Bem estar</button>
+          {allFrases.map(( item, index ) => (
+            <button 
+            key={item.id}
+            className="category-button"
+            style={{
+              borderWidth: item.nome === allFrases[categoriaSelecionada].nome ? 2 : 0,
+              borderColor: "#1fa4db"
+            }}
+            onClick={ () => handleSwitchCategory(index)}
+            >
+
+              {item.nome}
+            
+            </button>
+          ) )}
+        
         </section>
 
-        <button className="button-frase">Gerar frase</button>
+        <button className="button-frase" onClick={gerarFrase}>Gerar frase</button>
 
-        <p className="textoFrase">Alguma frase vai vir aqui</p>
+       {textofrase !== "" &&  <p className="textoFrase">{textofrase}</p>}
         
       </div> 
      
