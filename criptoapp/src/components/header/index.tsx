13647330abdbdbb855beedc26a2ed4074d59d302
Ivanclourenco <ivancampos039@gmail.com>
2025-02-06 19:39:49 -0300

    criando header

--- criptoapp/src/components/header/index.tsx
+++ criptoapp/src/components/header/index.tsx
@@ -1,7 +1,14 @@
+import styles from "./header.module.css"
+import logoimg from "../../assets/logo.svg"
+import { Link } from "react-router"
+
 export function Header(){
     return(
-        <header>
-            <h1>Header</h1>
+        <header className={styles.container}>
+            <Link to="/">
+            <img src={logoimg} alt="Logo criptoApp" />
+            </Link>
+            
         </header>
         
     )
--- criptoapp/src/pages/home/index.tsx
+++ criptoapp/src/pages/home/index.tsx
@@ -1,6 +1,8 @@
+import styles from "./home.module.css"
+
 export function Home(){
   return(
-    <div>
+    <div className={styles.container}>
       <h1>Página HOME</h1>
     </div>
   )
