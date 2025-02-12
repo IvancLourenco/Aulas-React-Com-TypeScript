4c656019db536b28ba17d6d4c7800b9c4507a249
Ivanclourenco <ivancampos039@gmail.com>
2025-02-11 21:03:31 -0300

    comecando a estilizacao e criando a pagina home

--- criptoapp/src/pages/home/index.tsx
+++ criptoapp/src/pages/home/index.tsx
@@ -1,9 +1,60 @@
 import styles from "./home.module.css"
+import {BsSearch} from "react-icons/bs"
+import{Link} from "react-router"
 
 export function Home(){
   return(
-    <div className={styles.container}>
-      <h1>Página HOME</h1>
-    </div>
+    <main className={styles.container}>
+      <form className={styles.form}>
+        <input
+         type="text"
+         placeholder="Digite o nome da moeda... Ex bitcoin"
+        />
+        <button type="submit">
+          <BsSearch size={30} color="#fff"/>
+        </button>
+      </form>
+
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Moeda</th>
+            <th scope="col">Valor mercado</th>
+            <th scope="col">Preço</th>
+            <th scope="col">Volume</th>
+            <th scope="col">Mudança 24 horas</th>
+          </tr>
+        </thead>
+
+        <tbody id="tbody">
+          <tr className={styles.tr}>
+            <td className={styles.tdlabel} data-Label= "Moeda">
+             <div className={styles.name}>
+             <Link to={"/detail/bitcoin"}>
+              <span>Bitcoin</span> | BTC
+             </Link>
+             </div>
+            </td>
+
+            <td className={styles.tdlabel} data-Label= "Valor mercado">
+              1T
+            </td>
+
+            <td className={styles.tdlabel} data-Label= "Preço">
+              8.000
+            </td>
+
+            <td className={styles.tdlabel} data-Label= "Volume">
+              2B
+            </td>
+
+            <td className={styles.tdlabel} data-Label= "Mudança 24 horas">
+               <span>1.20</span>
+            </td>
+
+          </tr>
+        </tbody>
+      </table>
+    </main>
   )
 }
